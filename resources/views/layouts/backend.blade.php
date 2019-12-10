9562f040060e629c37998979d79e1f80899d5b6c
edolorenza <edolrnz@gmail.com>
2019-12-10 20:19:47 +0700

    membuang search

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -96,17 +96,6 @@
                   <div class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
                     <button class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button sidebar-toggler">menu</button>
                     <span class="mdc-top-app-bar__title">Welcome, {{Auth::user()->name}}!</span>
-                    <div class="mdc-text-field mdc-text-field--outlined mdc-text-field--with-leading-icon search-text-field d-none d-md-flex">
-                      <i class="material-icons mdc-text-field__icon">search</i>
-                      <input class="mdc-text-field__input" id="text-field-hero-input">
-                      <div class="mdc-notched-outline">
-                        <div class="mdc-notched-outline__leading"></div>
-                        <div class="mdc-notched-outline__notch">
-                          <label for="text-field-hero-input" class="mdc-floating-label">Search..</label>
-                        </div>
-                        <div class="mdc-notched-outline__trailing"></div>
-                      </div>
-                    </div>
                   </div>
                   <div class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end mdc-top-app-bar__section-right">
                     <div class="menu-button-container menu-profile d-none d-md-block">
